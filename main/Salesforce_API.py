110941fa306198ffdc711a5e969be38faa3c4ec0
xj2193 <xj2193@cumc.columbia.edu>
2019-04-15 16:49:19 -0400

    Add Comments

--- main/Salesforce_API.py
+++ main/Salesforce_API.py
@@ -4,7 +4,7 @@ import pyodbc
 import pandas as pd
 import urllib
 
-
+# Connect to SQL Server --- AOUSQL
 def connect_sql_server():
     conn_str = (
         r'Driver=ODBC Driver 13 for SQL Server;'
@@ -16,23 +16,22 @@ def connect_sql_server():
     engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted_conn_str))
     return engine
 
-
+# Connect to Salesforce Sandbox (DataLoad)
 def connect_sf(password):
     sf = Salesforce(username='djb2188@cumc.columbia.edu.DataLoad', password=password,
                     security_token='IEEwFPkZVGLXjRkLQH1JyRlM', domain='test', version=42.0)
     return sf
 
-
+# Read SQL query and extract data from database
 def extract_data(engine, sql_file_name):
     connect = engine.connect()
-    #connect = connect_sql_server('aousql.dbmi.columbia.edu', 'dm_aou')[0]
     query = open(sql_file_name, 'r')
     sql_file = query.read()
     query.close()
     df = pd.read_sql_query(sql_file, connect)
     return df
 
-
+# Insert contact record into Salesforce Sandbox
 def insert_contact_sf(sf, record):
     count = 0
     for i in record:
@@ -43,7 +42,7 @@ def insert_contact_sf(sf, record):
             pass
     return print(str(count) + ' contacts inserted')
 
-
+# Insert journey record into Salesforce Sandbox
 def insert_journey_sf(sf, record):
     count = 0
     for i in record:
@@ -54,7 +53,7 @@ def insert_journey_sf(sf, record):
             pass
     return print(str(count) + ' journeys inserted')
 
-
+# Updade existing journey into Salesforce Sandbox
 def update_journey_sf(sf, record):
     count = 0
     for i in record:
@@ -67,8 +66,7 @@ def update_journey_sf(sf, record):
             pass
     return print(str(count) + ' journeys updated')
 
-
-def bulk_insert_sf(sf, record):
+'''def bulk_insert_sf(sf, record):
     count = 0
     try:
         sf.bulk.Contact.insert(record)
@@ -76,8 +74,9 @@ def bulk_insert_sf(sf, record):
     except:
         pass
     return print('Bulk insert ' + count + ' records')
+'''
 
-
+# Save datafrome into SQL Server table
 def save_data(df, engine, tablename):
     df.to_sql(tablename, engine, if_exists='replace', index=False)
     conn = engine.connect()
@@ -86,40 +85,34 @@ def save_data(df, engine, tablename):
 
 
 def main():
-    pw = 'DBM1@oU19'
+    pw = 'DBM1@oU19' # Sandbox password for Salesforce
     sf_api = connect_sf(pw)
-
-    contact_df = extract_data('sf code.sql')
+	# extract contact data from database and insert into Salesforce
+    contact_df = extract_data('sf_code.sql')
     contact_dict = contact_df.to_dict('records')
     insert_contact_sf(sf_api, contact_dict)
-
+    # pull Contact_Id and PMI Id from Salesforce and save data into database 
     query_contact = sf_api.query('SELECT Id, HP_PMI_ID__c FROM Contact')
     df_contact_pmi = pd.DataFrame(query_contact['records'])[['Id', 'HP_PMI_ID__c']]
     df_contact_pmi.columns = ['Contact_Id', 'PMI_Id']
     save_data(df_contact_pmi, 'Contact_PMI')
-
+    # extract Journey Id and Contact Id from Salesforce and save data into database
     query_journey = sf_api.query('SELECT Id, Contact__c FROM Opportunity')
     df_journey_contact = pd.DataFrame(query_journey['records'])[['Id', 'Contact__c']]
     df_journey_contact.columns = ['Journey_Id', 'Contact_Id']
     save_data(df_journey_contact, 'Contact_Journey')
-
+    # drop relationship table and insert new data
     engine = connect_sql_server()
     connect = engine.connect()
     query = open('Relationship.sql', 'r')
     sql_file = query.read()
     query.close()
     connect.execute(sql_file)
-
-    journey_df = extract_data(engine, 'journey sf code.sql')
+    # extract journey data from database and update into Salesforce
+    journey_df = extract_data(engine, 'journey_sf_code.sql')
     journey_df_merge = journey_df.merge(df_journey_contact)
-    #journey_df_merge = journey_df.merge(df_contact_pmi, how='left', on='HP_PMI_ID__c')
     journey_df_merge = journey_df_merge.drop(columns=['HP_PMI_ID__c'])
     journey_dict = journey_df_merge.to_dict('records')
-
     update_journey_sf(sf_api, journey_dict)
 
-
 main()
-
-
-
