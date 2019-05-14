a07e0e628081fa83b682a9495257726b05846241
xj2193 <xj2193@cumc.columbia.edu>
2019-05-14 11:17:17 -0400

    add result root into settings

--- _settings.py
+++ _settings.py
@@ -4,4 +4,5 @@ Database = 'name of the table where healthpro and redcap data is stored'
 sf_username = 'email_address.DataLoad/AoUPartial'
 sf_password = 'password'
 sf_security_token = 'security token for the environment'
+result_root = 'folder to save the result errors'
 
